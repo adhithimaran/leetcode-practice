ed7777d9d8e87b11e3183271bc4d4b12082717aa
adhithimaran <adhithimaran@gmail.com>
2025-01-16 21:08:46 -0600

    binary search fix: integer bin rep overflow, good

--- redo/BinarySearch.py
+++ redo/BinarySearch.py
@@ -1,14 +1,15 @@
-def search( nums, target):
-    left = 0
-    right = len(nums) -1
-    while (left <= right):
-        mid = (right + left) //2
-        if nums[mid] == target:
-            return mid            
-        elif nums[mid] < target:
-            left = mid + 1
-        else:
-            right = mid -1
+def search(nums, target):
+        l = 0
+        r = len(nums) - 1
+        while l <= r: 
+            mid = l + ((r-l) // 2)
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                l = mid+1
+            else:
+                r = mid -1
+        return -1
 
 # Input: 
 nums = [-1,0,2,4,6,8]
