49b32d1d47a7fb9b96d1fe56432ab83cbff15c5b
adhithimaran <adhithimaran@gmail.com>
2025-06-01 22:41:13 -0500

    Add print

--- arrays-hashing/HasDuplicate.py
+++ arrays-hashing/HasDuplicate.py
@@ -3,5 +3,6 @@ class HasDuplicate:
         for i in range(0, len(nums)-1):
             for j in range(i+1, len(nums)):
                 if (nums[i] == nums[j]):
+                    print(nums[i])
                     return True
         return False
