8f7738ffe3a5014a67207a0253500e48d523a6c5
adhithimaran <adhithimaran@gmail.com>
2025-06-20 22:38:47 -0500

    Reverse LL: Review this, made too complex

--- linked-list/ReorderLinkedList.py
+++ linked-list/ReorderLinkedList.py
@@ -14,3 +14,14 @@ class Solution:
             slow = slow.next
         
         middle = slow
+
+        list2 = middle.next 
+        curr = list2
+        # reverse 2nd LL
+        while (curr and curr.next):
+            next_node = curr.next
+            next_iter = curr.next.next
+            if (curr == list2): 
+                curr.next = None
+            next_node.next = curr
+            curr = next_node
