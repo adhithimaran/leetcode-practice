fa3d27f4900603f11a52eb5d1f0d3e31d5db9041
adhithimaran <adhithimaran@gmail.com>
2025-06-29 12:13:34 -0500

    Max Sum: Brute Force Solution

--- two-pointers/ContainerWithMostWater.py
+++ two-pointers/ContainerWithMostWater.py
@@ -1,15 +1,9 @@
 def maxArea(heights):
-    j = len(heights) - 1
-    max_sum = 0
+    res = 0
     for i in range(len(heights)):
-        temp_sum = 0
-        for k in range(i, j):
-            temp_sum += heights[k]
-            print(f'this is the end points: {heights[i]} and {heights[k]}')
-            print(f'sum: {temp_sum}')
-            if temp_sum > max_sum:
-                max_sum = temp_sum
-        j-=1
+        for j in range(i + 1, len(heights)):
+            res = max(res, min(heights[i], heights[j]) * (j - i))
+    return res
 
 height = [1,7,2,5,4,7,3,6]
 print(maxArea(height))
