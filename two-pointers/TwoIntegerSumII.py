beb0af6aae1d6e2bdc95e5521b741128a937354b
adhithimaran <adhithimaran@gmail.com>
2025-06-30 14:39:43 -0500

    Two Integer Sum II: Working Solution, Not Optimized

--- two-pointers/TwoIntegerSumII.py
+++ two-pointers/TwoIntegerSumII.py
@@ -1,17 +1,16 @@
-def twoSum(numbers, target):
-    # loop through array (2 ptrs)
-    left = 0
-    right = len(numbers)-1
-    while left != len(numbers)-1 or right != 0:
-        print(f'i = {numbers[left]}')
-        print(f'j = {numbers[right]}')
-        int_sum = numbers[left] + numbers[right]
-        if (int_sum == target):
-            return [numbers[left], numbers[right]]
-        elif (int_sum > target):
-            right -=1
-        else:
-            left +=1
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        # loop through array (2 ptrs)
+        left = 0
+        right = len(numbers)-1
+        while left < right:
+            int_sum = numbers[left] + numbers[right]
+            if (int_sum == target):
+                return [left+1, right+1]
+            elif (int_sum > target):
+                right -=1
+            else:
+                left +=1
 
 
 numbers=[2,3,4]
