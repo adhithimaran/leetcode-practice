601721c65111b7413a0d1e0d6f2f459ee84f30dd
adhithimaran <adhithimaran@gmail.com>
2025-07-01 15:28:02 -0500

    Valid Paren: Brute Force

--- stack/ValidParentheses.py
+++ stack/ValidParentheses.py
@@ -1,20 +1,8 @@
 def isValid(s):
-    if (len(s) % 2 != 0):
-        return False 
-    half = int(len(s)/2)
-    list1 = list(s[:half])
-    list2 = list(s[half:])
-    list2.reverse()
-    print(list1)
-    print(list2)
-    for i in range(len(list1)):
-        if (list1[i] == '{' and list2[i] != '}'):
-            return False
-        elif (list1[i] == '[' and list2[i] != ']'):
-            return False
-        elif (list1[i] == '(' and list2[i] != ')'):
-            return False
-    return True
+    while '()' in s or '{}' in s or '[]' in s:
+        s = s.replace('()', '')
+        s = s.replace('{}', '')
+        s = s.replace('[]', '')
 s = "([{}])"
 print(isValid(s))
 
