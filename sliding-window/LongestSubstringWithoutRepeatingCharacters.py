e9e422c656430beea27f920168cd5776f649a00d
adhithimaran <adhithimaran@gmail.com>
2025-07-07 22:38:54 -0500

    Longest Substring: working sol

--- sliding-window/LongestSubstringWithoutRepeatingCharacters.py
+++ sliding-window/LongestSubstringWithoutRepeatingCharacters.py
@@ -1,18 +1,22 @@
 def lengthOfLongestSubstring(s):
+    if not s:
+        return 0
+    
     i = 0
     j = 0
-    characters = []
-    lengths = 0
-
+    characters = set()
+    max_length = 0
+    
     while j < len(s):
-        if (s[j] in characters):
-            lengths = max(lengths, len(s[i:j]))
-            i+=1
-            j = i
-            continue
-        characters.append(s[j])
-        j+=1
-    return lengths
+        if s[j] not in characters:
+            characters.add(s[j])
+            j += 1
+            max_length = max(max_length, j - i)
+        else:
+            characters.remove(s[i])
+            i += 1
+    
+    return max_length
 
 
 print(lengthOfLongestSubstring("zxyzxyz"))
