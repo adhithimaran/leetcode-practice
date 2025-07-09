730168c14c1bec2f10c1df0e4513bc531bc16e5c
adhithimaran <adhithimaran@gmail.com>
2025-07-09 15:09:59 -0500

    Binary Search: V2

--- binary-search/BinarySearch.py
+++ binary-search/BinarySearch.py
@@ -6,18 +6,23 @@ def search(nums, target):
     high = len(nums)-1
     low = 0
     while (high >= low):
-        mid = (high - low) // 2
+        mid = low + (high - low) // 2
         print(f'this is high: {nums[high]}')
         print(f'this is low: {nums[low]}')
         print(f'this is mid: {nums[mid]}')
         print(f'current windoe: {nums[low:high+1]}')
         if (target < nums[mid]):
-            high = mid
+            high = mid-1
         elif (target > nums[mid]):
-            low = mid
+            low = mid+1
         else:
             return mid
+    return -1
+
+# nums = [-1,0,2,4,6,8]
+# target = 4
+# print(search(nums, target))
 
 nums = [-1,0,2,4,6,8]
-target = 4
+target = 3
 print(search(nums, target))
