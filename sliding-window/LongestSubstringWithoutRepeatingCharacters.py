8f60bf30af71dfcb54ba8e592be2cc199ca6249d
adhithimaran <adhithimaran@gmail.com>
2025-07-08 23:16:14 -0500

    Longest Substring: change list name

--- sliding-window/LongestSubstringWithoutRepeatingCharacters.py
+++ sliding-window/LongestSubstringWithoutRepeatingCharacters.py
@@ -4,16 +4,16 @@ def lengthOfLongestSubstring(s):
     
     i = 0
     j = 0
-    characters = set()
+    letters = set()
     max_length = 0
     
     while j < len(s):
-        if s[j] not in characters:
-            characters.add(s[j])
+        if s[j] not in letters:
+            letters.add(s[j])
             j += 1
             max_length = max(max_length, j - i)
         else:
-            characters.remove(s[i])
+            letters.remove(s[i])
             i += 1
     
     return max_length
