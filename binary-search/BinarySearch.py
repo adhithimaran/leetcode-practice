b5610248fcdd8cbbe2d81983dfc585ad3afccd14
adhithimaran <adhithimaran@gmail.com>
2025-07-09 15:15:56 -0500

    Binary Search: Optimizing Early Termination

--- binary-search/BinarySearch.py
+++ binary-search/BinarySearch.py
@@ -3,14 +3,14 @@
 # Implement a function to search for target within nums. If it exists, then return its index, otherwise, return -1.
 
 def search(nums, target):
+    if not nums:
+        return -1
+    if target > nums[-1] or target < nums[0]:
+        return -1
     high = len(nums)-1
     low = 0
     while (high >= low):
-        mid = low + (high - low) // 2
-        print(f'this is high: {nums[high]}')
-        print(f'this is low: {nums[low]}')
-        print(f'this is mid: {nums[mid]}')
-        print(f'current windoe: {nums[low:high+1]}')
+        mid = (low + high) // 2
         if (target < nums[mid]):
             high = mid-1
         elif (target > nums[mid]):
@@ -19,10 +19,10 @@ def search(nums, target):
             return mid
     return -1
 
-# nums = [-1,0,2,4,6,8]
-# target = 4
-# print(search(nums, target))
-
 nums = [-1,0,2,4,6,8]
-target = 3
+target = 4
 print(search(nums, target))
+
+# nums = [-1,0,2,4,6,8]
+# target = 3
+# print(search(nums, target))
