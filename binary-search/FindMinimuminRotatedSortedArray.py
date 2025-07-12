b79049887bf8de765ac516499aeddb289477149d
adhithimaran <adhithimaran@gmail.com>
2025-07-12 17:27:56 -0500

    Find Minimum in Rotated Sorted Array: Naive BF

--- binary-search/FindMinimuminRotatedSortedArray.py
+++ binary-search/FindMinimuminRotatedSortedArray.py
@@ -1,15 +1,5 @@
 def findMin(nums):
-    if not nums:
-        return -1
-    r = len(nums)-1
-    l = 0
-    while l < r:
-        m = (l + r) // 2
-        if nums[m] > nums[r]:  # Min is to the right
-            l = m + 1
-        else:  # Min is at mid or to the left
-            r = m
-    return nums[l]
+    return min(nums)
 
 nums1 = [3,4,5,6,1,2]
 nums2 = [4,5,0,1,2,3]
