1ded5661ae9b11613cfcba478a79735d682f9715
adhithimaran <adhithimaran@gmail.com>
2025-07-14 18:56:18 -0500

    search sorted rot array: working

--- binary-search/SearchinRotatedSortedArray.py
+++ binary-search/SearchinRotatedSortedArray.py
@@ -1,24 +1,31 @@
 def search(nums, target):
-    l, r = 0, len(nums)-1
+    l, r = 0, len(nums) - 1
+    
     while l <= r:
-        m = (r+l) // 2
-        print(f'low: {nums[l]}\nhigh: {nums[r]}\nmid: {nums[m]}\n')
-        if (nums[m] > nums[r] and nums[m] > target):
-            l = m+1
-        elif (nums[m] < nums[r] and nums[m] < target):
-            r = m-1
-        elif (nums[m] < nums[r] and nums[m] > target):
-            l = m+1
-        elif (nums[m] > nums[r] and nums[m] < target):
-            r = m-1
+        m = (l + r) // 2
         
+        # Check if we found the target
+        if nums[m] == target:
+            return m 
         
+        if nums[m] > target:
+            if nums[m] > nums[r]:
+                # right
+                l = m+1
+            else:
+                # left
+                r = m-1
+        
+        else:
+            if nums[m] > nums[r]:
+                # left
+                r = m-1
+            else:
+                #right
+                l = m+1
+
+    
     return -1
 
-nums = [3,4,5,6,1,2]
-target = 1
-#print(search(nums, target))
-
-nums = [3,5,6,0,1,2]
-target = 4
-print(search(nums, target))
+nums=[3,5,1]
+target=3
