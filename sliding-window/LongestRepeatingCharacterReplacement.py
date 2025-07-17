77e6209359d2bb0f802f0f9248c6fea2db00f9ff
adhithimaran <adhithimaran@gmail.com>
2025-07-17 09:06:20 -0500

    longest repl: bf

--- sliding-window/LongestRepeatingCharacterReplacement.py
+++ sliding-window/LongestRepeatingCharacterReplacement.py
@@ -4,20 +4,31 @@ def characterReplacement(s, k):
         # integer: how many letters I can replace 
     # output:
         # len: longest substring of repeating characters
-    max_length = 0
+    
+    # Case 1: added character is same
+        # Case 2: character not same but have k integer
+        # Case 3: character is not same , no more k int
+    max_len = 0
     left = 0
+    window_char_count = {}
     for right in range(len(s)):
-        window_char_count = {}
+        print(f'current window:\n{s[left:right+1]}')
         replacements = k
         valid = True
-        while valid:
-            if (s[right] not in window_char_count):
-                window_char_count[s[right]] = 1
-            else:
-                window_char_count[s[right]] += 1
-            # Case 1: added character is same
-            # Case 2: character not same but have k integer
-            # Case 3: character is not same , no more k int
+        if (s[right] not in window_char_count):
+            window_char_count[s[right]] = 1
+        else:
+            window_char_count[s[right]] += 1
+        
+        print(f'this is max key and value: {max(window_char_count.values())}')
+        print(f'this is length of current window: {len(s[left:right+1])}')
+        if (max(window_char_count.values()) + replacements) < len(s[left:right+1]):
+            valid = False
+            max_len = max(max_len, max(window_char_count.values()) + k)
+            window_char_count[s[left]] -=1
+            left +=1
+    return max_len
+        
 
 s = "XYYX"
 k = 2
