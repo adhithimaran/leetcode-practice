50421af155a84726a5ef79e5120b4490fed2f283
adhithimaran <adhithimaran@gmail.com>
2025-07-28 12:54:08 -0500

    Best sol

--- stack/DailyTemperatures.py
+++ stack/DailyTemperatures.py
@@ -4,21 +4,11 @@ def dailyTemperatures(temperatures):
         # pop elements from the stack until the top element of the stack is no longer less than the current element.
         # For each popped element, we compute the difference between the indices and store it in the position corresponding to the popped element
     
-    stack_temps = temperatures.copy()
-    res = []
-    for i in range(1, len(temperatures)):
-        print(f'bigger for loop: {temperatures[i]}')
-        if temperatures[i] <= temperatures[i-1]:
-            continue
-        print(f'larger element: {temperatures[i]}')
-        count = 0
-        while count < i:
-            temp = stack_temps.pop(0)
-            print(f'stack ele poper off: {temp}')
-            res.append(count+1)
-            print(f'stack: \n{stack_temps}')
-            count +=1
-    return res
-
-temperatures = [30,38,30,36,35,40,28]
-print(dailyTemperatures(temperatures))
+    res = [0] * len(temperatures)  
+    stack = []  
+    
+    for i in range(len(temperatures)):
+        while stack and temperatures[stack[-1]] < temperatures[i]:
+            j = stack.pop()
+            res[j] = i - j
+        stack.append(i)
