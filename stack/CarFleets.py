87d9ad28c2f3cede2b952ae18187be340ef3637a
adhithimaran <adhithimaran@gmail.com>
2025-07-29 22:25:37 -0500

    Car Fleets: Working Version

--- stack/CarFleets.py
+++ stack/CarFleets.py
@@ -1,16 +1,25 @@
 def carFleet(target, position, speed):
     n = len(position)
-    fleet_times = [] # stack DS
-    position.sort(reverse=True)
-    print(f'this is positions array sorted: \n{position}')
-
-    for i in range(len(position)):
-        time_to_target = (target - position[i]) / speed[i]
-        print(f'this is time to target: \n{time_to_target}')
-
-        if fleet_times == [] or time_to_target > fleet_times[-1]:
+    
+    # Create pairs of (position, speed) and sort by position in descending order
+    cars = list(zip(position, speed))
+    cars.sort(reverse=True)  # Sort by position (descending)
+    
+    print(f'Cars sorted by position (desc): {cars}')
+    
+    fleet_times = []  # stack to track fleet arrival times
+    
+    for pos, spd in cars:
+        time_to_target = (target - pos) / spd
+        print(f'Car at position {pos} with speed {spd}: time = {time_to_target}')
+        
+        # If this car takes longer than the previous fleet, it forms a new fleet
+        if not fleet_times or time_to_target > fleet_times[-1]:
             fleet_times.append(time_to_target)
-
+            print(f'New fleet formed. Fleet times: {fleet_times}')
+        else:
+            print(f'Car joins existing fleet (time {time_to_target} <= {fleet_times[-1]})')
+    
     return len(fleet_times)
 
 
