0f3fb8e26208453bb5060b227227d80b8343f374
adhithimaran <adhithimaran@gmail.com>
2025-08-04 02:21:23 -0500

    LRU Cache: V3 bf working

--- linked-list/LRUCache.py
+++ linked-list/LRUCache.py
@@ -7,13 +7,23 @@ class LRUCache:
 
     def get(self, key: int) -> int:
         if key in self.LRU_map:
+            if key in self.last_updated:
+                self.last_updated.remove(key)
             self.last_updated.append(key)
             return self.LRU_map[key]
         return -1
 
     def put(self, key: int, value: int) -> None:
+        if key in self.LRU_map:
+            self.LRU_map[key] = value
+            if key in self.last_updated:
+                self.last_updated.remove(key)
+            self.last_updated.append(key)
+            return
         if len(self.LRU_map) >= self.capacity:
             least_used = self.last_updated.pop(0)
             del self.LRU_map[least_used]
         self.LRU_map[key] = value
+        if key in self.last_updated:
+            self.last_updated.remove(key)
         self.last_updated.append(key)
