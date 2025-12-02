20d5bf0c59a84cd5071429d37f56de7b51e6f2da
adhithimaran <adhithimaran@gmail.com>
2025-12-02 14:33:47 -0600

    Add: 290 - Word Pattern (easy)

---
+++ arrays-hashing/word__pattern.py
@@ -0,0 +1,24 @@
+"""
+LeetCode #290: Word Pattern
+Difficulty: Easy
+Topic: hash
+
+Problem:
+
+
+Solution:
+
+"""
+
+class Solution(object):
+    def solve(self):
+        """
+        :rtype: 
+        """
+        pass
+
+
+# Test cases
+if __name__ == "__main__":
+    sol = Solution()
+    # Add test cases here
