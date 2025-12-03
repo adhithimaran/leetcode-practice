a9beb09554e77e3952b7d7d763eb4ff80a855db7
adhithimaran <adhithimaran@gmail.com>
2025-12-03 13:29:48 -0600

    Add: 73 - Set Matriz Zeroes (medium)

---
+++ arrays-hashing/set__matriz__zeroes.py
@@ -0,0 +1,24 @@
+"""
+LeetCode #73: Set Matriz Zeroes
+Difficulty: Medium
+Topic: arrays
+
+Problem:
+
+
+Solution:
+
+"""
+
+class Solution(object):
+    def solve(self):
+        """
+        :rtype: 
+        """
+        pass
+
+
+# Test cases
+if __name__ == "__main__":
+    sol = Solution()
+    # Add test cases here
