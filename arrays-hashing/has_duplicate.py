6db9a9bba963eaf6eb360fe20fd3930f86cda3ba
adhithimaran <adhithimaran@gmail.com>
2026-01-17 17:23:20 -0600

    Has Duplicate: Easy

--- arrays-hashing/has_duplicate.py
+++ arrays-hashing/has_duplicate.py
@@ -1,8 +1,10 @@
-class HasDuplicate:
-    def hasDuplicate(self, nums: List[int]) -> bool:
-        for i in range(0, len(nums)-1):
-            for j in range(i+1, len(nums)):
-                if (nums[i] == nums[j]):
-                    print(nums[i])
-                    return True
-        return False
+def hasDuplicate(self, nums: List[int]) -> bool:
+       # BF: iterate through nums n times and check if element appears twice, return true
+       # O(n squared)
+       nums.sort()
+       for i in range(len(nums)-1):
+            curr = nums[i]
+            next_ele = nums[i+1]
+            if (curr == next_ele):
+                return True
+       return False
