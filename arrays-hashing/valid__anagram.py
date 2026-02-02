98e59ce00502020269d0cdff3eb902bead70b247
Adhithi Maran <adhithimaran@gmail.com>
2026-02-02 14:12:56 -0600

    Add: 242 - Valid Anagram (easy)

---
+++ arrays-hashing/valid__anagram.py
@@ -0,0 +1,24 @@
+"""
+LeetCode #242: Valid Anagram
+Difficulty: Easy
+Topic: Arrays
+
+Problem:
+
+
+Solution:
+
+"""
+
+class Solution(object):
+    def solve(self):
+        """
+        :rtype: 
+        """
+        pass
+
+
+# Test cases
+if __name__ == "__main__":
+    sol = Solution()
+    # Add test cases here
