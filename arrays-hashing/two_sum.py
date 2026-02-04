6e56978292539d21756f1f5aea4a39e6834b03ae
Adhithi Maran <adhithimaran@gmail.com>
2026-02-04 14:38:09 -0600

    Review: 1 (now confident)

--- arrays-hashing/two_sum.py
+++ arrays-hashing/two_sum.py
@@ -1,9 +1,19 @@
-class TwoSum:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)-1):
-            for j in range(i+1, len(nums)):
-                if ((nums[i] + nums[j] == target) and (i != j)):
-                    if (i < j):
-                        return [i, j]
-                    else:
-                        return [j, i]
+def twoSum(nums, target):
+    i, j = 0, 1
+    while i < len(nums)-1:
+        ith = nums[i]
+        jth = nums[j]
+        if ith + jth == target:
+            return [i, j]
+
+        if j != len(nums)-1:
+            j += 1
+        else:
+            i += 1
+            j = i+1
+
+
+nums = [5,5]
+target = 10
+
+print(twoSum(nums, target))
