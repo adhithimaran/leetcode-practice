40a08096024716c661b976af46b2d7350c54af5c
Adhithi Maran <adhithimaran@gmail.com>
2026-03-30 14:53:54 -0500

    Change: var name

--- arrays-hashing/max_profit.py
+++ arrays-hashing/max_profit.py
@@ -1,5 +1,5 @@
 def maxProfit(self, prices: List[int]) -> int:
-    max_profit = 0
+    max_prof = 0
     i, j = 0, 1
 
     while i < len(prices) - 1:
@@ -7,10 +7,10 @@ def maxProfit(self, prices: List[int]) -> int:
         sell = prices[j]
         curr_profit = sell - buy
         if (curr_profit >= 0):
-            max_profit = max(curr_profit, max_profit)
+            max_prof = max(curr_profit, max_prof)
 
         j += 1
         if j == len(prices):
             i += 1
             j = i + 1
-    return max_profit
+    return max_prof
